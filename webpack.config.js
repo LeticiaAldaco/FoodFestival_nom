d96776b56eea9b23f887abb3c0c60e6410586673
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-08-11 20:52:57 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require("webpack");
+
 const path = require("path");
 module.exports = {
   entry: "./assets/js/script.js",
@@ -5,5 +7,11 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "main.bundle.js",
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+  ],
   mode: "development",
 };
